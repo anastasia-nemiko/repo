c9ea42338de6cad73e0383ba62601bfc272f71d8
Nastia <anastasia.nemiko@gmail.com>
2018-11-21 17:55:39 +0200

    first commit

---
+++ 1 lesson/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+
+var LeftBorderWidth = 1;
+{
+    let second = 2;
+}
+const pi = 3.14;
+
+let person = {
+    name: 'Nastia',
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[0]);
+
+//alert ("Javascript Ebanyj!");
+
+//let answer = confirm('Are you here?');
+
+//console.log(answer);
+
+//let answer =  prompt('javascript ebanyj?','Da');
+
+//console.log(typeof(answer));
+
+//console.log(' arr '+ 4);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+
